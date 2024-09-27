981cb79a7c77334e40eaf7cb27a7dc57be36f36b
ggg <seokwoo.chung130@gmail.com>
2024-09-27 17:33:36 +0900

    fix path bug in data_collection.py

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 def download_data(ticker, period='5y', interval='1d'):
     data = yf.download(ticker, period=period, interval=interval)
-    data.to_csv(f'../data/{ticker}_data.csv')
+    data.to_csv(f'./data/{ticker}_data.csv')
     print(f'Data for {ticker} downloaded successfully.')
 
 if __name__ == '__main__':
